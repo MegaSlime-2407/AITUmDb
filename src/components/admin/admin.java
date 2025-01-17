700f32b03820eb42fb06716df7bd017f732e9d9b
alinur229035@gmail.com <alinur229035@gmail.comgit config --global user.email alinur229035@gmail.com>
2025-01-17 15:03:52 +0500

    origin

--- src/components/admin/admin.java
+++ src/components/admin/admin.java
@@ -1,4 +1,31 @@
 package components.admin;
 
 public class admin {
+    private int id;
+    private String name;
+    private String password;
+
+    public admin(int id, String name, String password) {
+        setId(id);
+        setName(name);
+        setPassword(password);
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
