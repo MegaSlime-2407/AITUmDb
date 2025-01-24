ee0b3c41e1bb9d16f640be47563d2b09849c9933
aldik1607 <aldiyar.tandau2007@gmail.com>
2025-01-24 14:52:28 +0500

    added get by id, get all methods

--- src/components/admin/Admin.java
+++ src/components/admin/Admin.java
@@ -1,6 +1,11 @@
 package components.admin;
 
-public class Admin {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Admin implements open{
     private int id;
     private String name;
     private String password;
@@ -28,4 +33,44 @@ public class Admin {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public void getAll() {
+        String query = "SELECT * FROM films";
+        try (Connection connection = DatabaseConnection.connect();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            System.out.println("All films:");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String director = resultSet.getString("director");
+                System.out.println("ID: " + id + ", Title: " + title + ", Director: " + director);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void getById(int filmId) {
+        String query = "SELECT * FROM films WHERE id = " + filmId;
+        try (Connection connection = DatabaseConnection.connect();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String director = resultSet.getString("director");
+                System.out.println("Film found: ID: " + id + ", Title: " + title + ", Director: " + director);
+            } else {
+                System.out.println("Film not found with ID: " + filmId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
 }
