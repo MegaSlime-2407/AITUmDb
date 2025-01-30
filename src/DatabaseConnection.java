95effe9ca1ba6a6d0168faf18273f868ea88ff65
aliiyaaa <alfiagalidullaeva@gmail.com>
2025-01-30 15:09:01 +0300

    created UserServices class

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/aitumdb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "0000";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/aitumdb";
-        String user = "postgres";
-        String password = "0000";
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database.");
-            AdminService adminService = new AdminService(connection);
-            UserServices userServices = new UserServices(connection);
-            Scanner scanner = new Scanner(System.in);
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            if(connection != null) {
+                System.out.println("Connected to the database.");
+                AdminService adminService = new AdminService(connection);
+                UserServices userServices = new UserServices(connection);
+                Scanner scanner = new Scanner(System.in);
 
-            Menu menu = new Menu(adminService, userServices, scanner);
-            menu.displayMainMenu();
+                Menu menu = new Menu(adminService, userServices, scanner);
+                menu.displayMainMenu();
+            }
+            else System.out.printf("You are not connected to the database.%n");
 
         } catch (SQLException e) {
             System.out.println("Connection error:");
